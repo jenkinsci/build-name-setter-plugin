b1b3aeb70cbf5033f3ffc0f8191ec79b52ea7350
lavaliere <kennedy.tracy@yahoo.com>
2015-02-26 15:30:28 -0500

    Fixed style issues caused by Eclipse

--- src/main/java/org/jenkinsci/plugins/buildnamesetter/BuildNameSetter.java
+++ src/main/java/org/jenkinsci/plugins/buildnamesetter/BuildNameSetter.java
@@ -38,7 +38,6 @@ public class BuildNameSetter extends BuildWrapper implements MatrixAggregatable 
         this.template = template;
         this.matrixTemplate = matrixTemplate;
         matches = template.equals(matrixTemplate);
-        
         if(matches || matrixTemplate.length()>0 && template.length()==0){
         	scenario = 0;
         }else if (matrixTemplate.length()>0 && template.length()>0){
@@ -57,21 +56,21 @@ public class BuildNameSetter extends BuildWrapper implements MatrixAggregatable 
     @Override
     public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
         try{
-        	//not checking before setting name because this check is done via Jelly call to isMatrix()
-        	setDisplayName(build, listener);
+            //not checking before setting name because this check is done via Jelly call to isMatrix()
+            setDisplayName(build, listener);
         }catch(Exception e){
-        	e.printStackTrace();
-        	return null;
+            e.printStackTrace();
+            return null;
         }
     	return new Environment() {
             @Override
             public boolean tearDown(AbstractBuild build, BuildListener listener) throws IOException, InterruptedException {
                 try {
-					setDisplayName(build, listener);
-	                return true;
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+                    setDisplayName(build, listener);
+	            return true;
+		} catch (Exception e) {
+                    e.printStackTrace();
+                }
                 return false;
             }
     	};
@@ -79,23 +78,23 @@ public class BuildNameSetter extends BuildWrapper implements MatrixAggregatable 
 
     private void setDisplayName(AbstractBuild build, BuildListener listener) throws Exception {
     	try{
-    		switch(scenario){
-    		case 0:
-    	    		build.setDisplayName(TokenMacro.expandAll(build, listener, matrixTemplate));
-    				return;
-    		case 1:
-	    			if(started){	
-	    				build.setDisplayName(TokenMacro.expandAll(build, listener, matrixTemplate));
-	    			}else{
-	    				build.setDisplayName(TokenMacro.expandAll(build, listener, template));
-	    			}
-    				return;
-    		case 2:
-	    			build.setDisplayName(TokenMacro.expandAll(build, listener, template));
-    				return;
-    		case -1:
-    				throw new Exception("No valid values set for build names!");
-    		}
+            switch(scenario){
+            case 0:         
+                build.setDisplayName(TokenMacro.expandAll(build, listener, matrixTemplate));
+                return;
+            case 1:
+                if(started){	
+                    build.setDisplayName(TokenMacro.expandAll(build, listener, matrixTemplate));
+                }else{
+                    build.setDisplayName(TokenMacro.expandAll(build, listener, template));
+                }
+                return;
+            case 2:
+                build.setDisplayName(TokenMacro.expandAll(build, listener, template));
+                return;
+            case -1:
+                throw new Exception("No valid values set for build names!");
+            }
     	} catch (MacroEvaluationException e) {
           listener.getLogger().println(e.getMessage());
     	}  
@@ -106,22 +105,22 @@ public class BuildNameSetter extends BuildWrapper implements MatrixAggregatable 
             @Override
             public boolean startBuild() throws InterruptedException, IOException {
                 try {
-					setDisplayName(build,listener);
-					started = true;
-	                return super.startBuild();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+                    setDisplayName(build,listener);
+                    started = true;
+	            return super.startBuild();
+		} catch (Exception e) {
+                    e.printStackTrace();
+		}
                 return false;
             }
 
             @Override
             public boolean endBuild() throws InterruptedException, IOException {
             	try{
-            		setDisplayName(build,listener);
-            		return super.endBuild();
+                    setDisplayName(build,listener);
+                    return super.endBuild();
             	}catch(Exception e){
-            		e.printStackTrace();
+                    e.printStackTrace();
             	}
             	return false;
             }
