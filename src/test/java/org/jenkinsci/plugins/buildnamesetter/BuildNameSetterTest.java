b38f1613e1615ebbd5936bb9cbb01aa6d152dd6f
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-10 16:10:22 +0100

    Migrate tests to JUnit5 (#142)
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/plugins/buildnamesetter/BuildNameSetterTest.java
+++ src/test/java/org/jenkinsci/plugins/buildnamesetter/BuildNameSetterTest.java
@@ -1,9 +1,7 @@
 package org.jenkinsci.plugins.buildnamesetter;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import hudson.EnvVars;
 import hudson.model.FreeStyleBuild;
@@ -11,19 +9,16 @@ import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import hudson.model.TaskListener;
 import org.jenkinsci.plugins.EnvironmentVarSetter;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-public class BuildNameSetterTest {
-
-    @Rule
-    public JenkinsRule jenkins = new JenkinsRule();
+@WithJenkins
+class BuildNameSetterTest {
 
     @Test
-    public void shouldExpand_BUILD_NUMBER_macro() throws Exception {
+    void shouldExpand_BUILD_NUMBER_macro(JenkinsRule jenkins) throws Exception {
         FreeStyleProject fooProj = jenkins.createFreeStyleProject("foo");
         fooProj.getBuildWrappersList().add(getDefaultSetter("a_#${BUILD_NUMBER}"));
 
@@ -32,7 +27,7 @@ public class BuildNameSetterTest {
     }
 
     @Test
-    public void shouldExpand_JOB_NAME_full_env_macro() throws InterruptedException, ExecutionException, IOException {
+    void shouldExpand_JOB_NAME_full_env_macro(JenkinsRule jenkins) throws Exception {
         FreeStyleProject barProj = jenkins.createFreeStyleProject("bar");
         barProj.getBuildWrappersList().add(getDefaultSetter("b_${ENV,var=\"JOB_NAME\"}"));
 
@@ -42,7 +37,7 @@ public class BuildNameSetterTest {
 
     @Issue("13347")
     @Test
-    public void shouldExpand_JOB_NAME_macro() throws InterruptedException, ExecutionException, IOException {
+    void shouldExpand_JOB_NAME_macro(JenkinsRule jenkins) throws Exception {
         FreeStyleProject barProj = jenkins.createFreeStyleProject("bar");
         barProj.getBuildWrappersList().add(getDefaultSetter("c_${JOB_NAME}"));
 
@@ -52,7 +47,7 @@ public class BuildNameSetterTest {
 
     @Issue("13347")
     @Test
-    public void shouldExpand_JOB_NAME_macro_twice() throws InterruptedException, ExecutionException, IOException {
+    void shouldExpand_JOB_NAME_macro_twice(JenkinsRule jenkins) throws Exception {
         FreeStyleProject barProj = jenkins.createFreeStyleProject("bar");
         barProj.getBuildWrappersList().add(getDefaultSetter("c_${JOB_NAME}_d_${JOB_NAME}"));
 
@@ -62,7 +57,7 @@ public class BuildNameSetterTest {
 
     @Issue("13347")
     @Test
-    public void shouldExpand_NODE_NAME_macro_and_JOB_NAME_full_env_macro() throws InterruptedException, ExecutionException, IOException {
+    void shouldExpand_NODE_NAME_macro_and_JOB_NAME_full_env_macro(JenkinsRule jenkins) throws Exception {
         FreeStyleProject fooProj = jenkins.createFreeStyleProject("foo");
         fooProj.getBuildWrappersList().add(getDefaultSetter("d_${NODE_NAME}_${ENV,var=\"JOB_NAME\"}"));
 
@@ -72,7 +67,7 @@ public class BuildNameSetterTest {
 
     @Issue("34181")
     @Test
-    public void shouldUse_default_config_values_if_null() throws InterruptedException, ExecutionException, IOException {
+    void shouldUse_default_config_values_if_null(JenkinsRule jenkins) throws Exception {
         FreeStyleProject fooProj = jenkins.createFreeStyleProject("foo");
         fooProj.getBuildWrappersList().add(new BuildNameSetter("${ENV,var=\"JOB_NAME\"}", null, null));
 
@@ -80,22 +75,18 @@ public class BuildNameSetterTest {
         assertDisplayName(fooBuild, "foo");
     }
 
-    private void assertDisplayName(FreeStyleBuild build, String expectedName) {
+    private static void assertDisplayName(FreeStyleBuild build, String expectedName) {
         assertEquals(Result.SUCCESS, build.getResult());
         assertEquals(expectedName, build.getDisplayName());
         EnvironmentVarSetter action = build.getAction(EnvironmentVarSetter.class);
         assertEquals(expectedName, action.getVar(EnvironmentVarSetter.buildDisplayNameVar));
-        EnvVars envVars = null;
-        try {
-            envVars = build.getEnvironment(TaskListener.NULL);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail("Exception was thrown during getting build environment:" + e.getMessage());
-        }
+        EnvVars envVars = assertDoesNotThrow(
+                () -> build.getEnvironment(TaskListener.NULL),
+            "Exception was thrown during getting build environment");
         assertEquals(expectedName, envVars.get(EnvironmentVarSetter.buildDisplayNameVar));
     }
 
-    private BuildNameSetter getDefaultSetter(String template) {
+    private static BuildNameSetter getDefaultSetter(String template) {
         return new BuildNameSetter(template, true, true);
     }
 }
